436415e77e544522072d35e9bd37f1fac7ac26c8
Renan Marchetto <rmarchetto@ndt-global.com>
2025-04-09 20:25:49 -0400

    Commit Code to Git for testing

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+class Program
+{
+  static async Task Main()
+  {
+    await ListPokemonsAsync();
+  }
+
+  static async Task ListPokemonsAsync()
+  {
+    string url = "https://pokeapi.co/api/v2/pokemon?limit=10"; // Fetch 10 Pokémons
+
+    using HttpClient client = new HttpClient();
+    HttpResponseMessage response = await client.GetAsync(url);
+
+    if (response.IsSuccessStatusCode)
+    {
+      string json = await response.Content.ReadAsStringAsync();
+      JObject data = JObject.Parse(json);
+
+      foreach (var item in data["results"])
+      {
+        Console.WriteLine($"Name: {item["name"]}");
+      }
+    }
+    else
+    {
+      Console.WriteLine("Error fetching data.");
+    }
+  }
+}
